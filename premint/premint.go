0785e57fa3fb174b8715d68131eca81dce273109
Mager <andrew.mager@gmail.com>
2022-04-15 06:27:47 -0500

    Attempt to fix API URL

--- premint/premint.go
+++ premint/premint.go
@@ -53,7 +53,7 @@ var Options = ProvidePremint
 // https://www.premint.xyz/api/6c808f0c6964bd664854f051b438ca8b3aafec70938e5deca6ab49662a789cac/discord/360541062839926785
 func CheckPremintStatusForUser(logger *zap.SugaredLogger, apiKey, userID string) (CheckPremintStatusResp, error) {
 	r := CheckPremintStatusResp{}
-	url := fmt.Sprintf("https://www.premint.xyz/api/%s/discord/%s", apiKey, userID)
+	url := fmt.Sprintf("https://api.premint.xyz/v1/%s/discord/%s", apiKey, userID)
 
 	logger.Infow("Calling Premint API with discord user ID", "url", url)
 
@@ -84,7 +84,7 @@ func CheckPremintStatusForUser(logger *zap.SugaredLogger, apiKey, userID string)
 // Example: https://www.premint.xyz/api/6c808f0c6964bd664854f051b438ca8b3aafec70938e5deca6ab49662a789cac/wallet/0x064DcA21b1377D1655AC3CA3e95282D9494E5611
 func CheckPremintStatusForAddress(logger *zap.SugaredLogger, apiKey, address string) (CheckPremintStatusResp, error) {
 	r := CheckPremintStatusResp{}
-	url := fmt.Sprintf("https://www.premint.xyz/api/%s/wallet/%s", apiKey, address)
+	url := fmt.Sprintf("https://api.premint.xyz/v1/%s/wallet/%s", apiKey, address)
 
 	logger.Infow("Calling Premint API with wallet address", "url", url)
 
